6a5f761fb6f052e32ccd430107eb55f524282a9d
Petr Koláček <petr.kolacek@etnetera.cz>
2018-10-18 15:12:41 +0200

    reqres - fetch users, add user

--- JS/jQuery-AJAX/reqres.js
+++ JS/jQuery-AJAX/reqres.js
@@ -1,24 +1,100 @@
 $(function (){
 
-  var $usersContainer = $('.users-container')
-  var $requestUrl = 'https://reqres.in/api/users/'
+  var $usersContainer = $('.users-container');
+  var $infoContainer = $('.info-container');
+  var $requestUrl = 'https://reqres.in/api/users/';
+  var $currentPage = 1;
+  var $totalPages = 0;
+
+
+  /**
+   * Add new user
+   */
+
+  $('.add-user').on("click", function(){
+    var name = $('#userName').val();
+    var job = $('#userJob').val()
+    addUser(name, job);
+  });
+
+  function addUser(name, job) {
+    $.ajax({
+      type: 'POST',
+      url: $requestUrl,
+      data: {
+        name: name,
+        job: job,
+      },
+      success: function(user) {
+        console.log(user);
+      },
+      error: function() {
+        console.error('Error adding user');
+      }
+    });
+  }
+
+
+  /**
+   * User list
+   */
+
+  var userTemplate = "" +
+  "<div>" +
+    "<h3>{{first_name}} {{last_name}}</h3>" +
+    "<div><img src='{{avatar}}' /></div>" +
+  "</div>" +
+  "";
+
+
+  $('.link-next').on("click", function(){
+    if($currentPage < $totalPages) {
+      $currentPage++;
+      $usersContainer.empty();
+      fetchUsers();
+      printInfo();
+    }
+  });
+
+  $('.link-prev').on("click", function(){
+    if($currentPage > 1) {
+      $currentPage--;
+      $usersContainer.empty();
+      fetchUsers();
+      printInfo();
+    }
+  });
 
   function printUser(user) {
-    $usersContainer.append('<h2>' + user.first_name + ' ' + user.last_name + '</h2>');
+    $usersContainer.append(Mustache.render(userTemplate, user));
   }
+
+  function printInfo() {
+    $infoContainer.empty();
+    $infoContainer.append('page: ' + $currentPage + ' / total pages: ' + $totalPages);
+  }
+
+
 
   function fetchUsers() {
     $.ajax({
       type: 'GET',
       url: $requestUrl,
       data: {
-        page: 1,
+        page: $currentPage,
       },
       success: function(users) {
         console.log(users);
         $.each(users.data, function(i, user){
           printUser(user);
         });
+
+        $currentPage = users.page;
+        $totalPages = users.total_pages;
+        printInfo();
+        console.log('current page: ' + $currentPage);
+        console.log('total pages: ' + $totalPages);
+
       },
       error: function() {
         console.error("Error fetching data ...");
