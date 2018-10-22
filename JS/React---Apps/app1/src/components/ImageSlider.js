ac1d1d4446ddfdab6806a39d13b4f37ed70fd8df
Petr Koláček <petr.kolacek@etnetera.cz>
2018-10-22 14:26:41 +0200

    Image slider - vylepšení

--- JS/React---Apps/app1/src/components/ImageSlider.js
+++ JS/React---Apps/app1/src/components/ImageSlider.js
@@ -1,49 +1,46 @@
 import React from "react";
 
-/*
-  1. shallow merge
-  2. async
-*/
-
-export default class ImageSlider extends React.Component {
+class ImageSlider extends React.Component {
   state = {
     images: [
-      "https://i.pinimg.com/236x/b6/21/07/b62107d70659823c6f409480f7435d14--watercolor-food-watercolor-print.jpg",
-      "https://i.pinimg.com/236x/8a/61/c1/8a61c1ca0d1a20db922aa5e3a8493d0e--watercolour-flowers-watercolor-ideas.jpg",
-      "https://i.pinimg.com/236x/86/d6/46/86d646ce2f92efd7890843ef5af9e700.jpg",
-      "https://i.pinimg.com/236x/f6/4d/d7/f64dd7eb8df298195d61569b9634f982--watercolor-sketch-watercolor-kiwi.jpg"
+      "https://picsum.photos/g/600/300?image=0",
+      "https://picsum.photos/g/600/300?image=1",
+      "https://picsum.photos/g/600/300?image=2",
+      "https://picsum.photos/g/600/300?image=3",
+      "https://picsum.photos/g/600/300?image=4",
+      "https://picsum.photos/g/600/300?image=5",
     ],
     idx: 0
   };
 
   handleNext = () => {
-    this.setState({
-      idx: this.state.idx + 1
-    });
+    if(this.state.idx < this.state.images.length - 1) {
+      this.setState({
+        idx: this.state.idx + 1
+      });
+    }
+  };
+
+  handlePrev = () => {
+    if(this.state.idx > 0) {
+      this.setState({
+        idx: this.state.idx - 1
+      });
+    }
   };
 
   render() {
     return (
       <div>
-        <button
-          onClick={() => {
-            this.setState({
-              idx: this.state.idx - 1
-            });
-          }}
-        >
-          previous
-        </button>
-        <img
-          alt=""
-          style={{
-            width: 100,
-            height: 100
-          }}
-          src={this.state.images[this.state.idx]}
-        />
-        <button onClick={this.handleNext}>next</button>
+          <p>{this.state.idx} / {this.state.images.length}</p>
+          <p>
+            <button onClick={this.handlePrev}>prev</button>
+            <button onClick={this.handleNext}>next</button>
+          </p>
+          <p><img src={this.state.images[this.state.idx]} alt={this.state.idx} /></p>
       </div>
     );
   }
 }
+
+export default ImageSlider;
