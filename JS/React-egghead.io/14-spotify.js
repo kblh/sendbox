44897130b778039468fea439858015d3c506069e
Petr Koláček <petr.kolacek@etnetera.cz>
2019-11-07 16:11:44 +0100

    index

--- JS/React-egghead.io/14-spotify.js
+++ JS/React-egghead.io/14-spotify.js
@@ -8,10 +8,11 @@ class FetchSpotify extends React.Component {
   }
 
   async componentDidMount() {
-    const url = "https://api.spotify.com/v1/playlists/7JLS9yCu9JH2gDJZur70Jx/tracks?fields=items.track.album";
+    const url = "https://api.spotify.com/v1/users/kblh/playlists"
+    // const url = "https://api.spotify.com/v1/playlists/7JLS9yCu9JH2gDJZur70Jx/tracks?fields=items.track.album";
     const accessToken = "BQBgF2uUbHYYKZWBefkP1ClYuKmVdaecOI9uhkNpIqGUBrB3cf83c4VOJTMqXid--ld2tQYclOYW7gtwLzPf05BpCivnyMN1B--09WakV42Gt6n1gL6nQBkhkXGa9x6SUm49PWjKNplA2lw31lqShC4EXflNkSkjsw2C2qiJ9LqWPvzKDazRS5gfcv7G6SpZ4dVN4hd8ZmvO4SQlfJqF";
-    // const response = await fetch(url);
-    // const data = await response.json();
+    const response = await fetch(url);
+    const data = await response.json();
 
     await fetch(url, {
       headers: {'Authorization': 'Bearer' + accessToken}
